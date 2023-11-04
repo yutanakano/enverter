538cdbd0fac902d5e59eafda38f279054b7b71b0
yutanakano <yutanakano395@icloud.com>
2023-11-05 03:22:29 +0900

    :wrench: NodeをLTSのみに変更した

--- src/asdf/init.sh
+++ src/asdf/init.sh
@@ -6,9 +6,12 @@ asdf plugin add nodejs
 # バージョンの追加
 asdf install rust 1.67.1
 asdf install python 3.11.0
-asdf install nodejs 16.20.0
-asdf install nodejs 18.14.0
+asdf install nodejs 16.20.2
+asdf install nodejs 18.18.2
+asdf install nodejs 19.9.0
+asdf install nodejs 20.9.0
+asdf install nodejs 21.1.0
 # globalに設定
 asdf global rust 1.67.1
 asdf global python 3.11.0
-asdf global nodejs 16.20.0
+asdf global nodejs 21.1.0
