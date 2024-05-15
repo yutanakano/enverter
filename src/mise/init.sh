0ddb7ef921f9384a49ce6898b7834ad91cdb12ff
yutanakano <yutanakano395@icloud.com>
2024-05-15 21:53:33 +0900

    :wrench: グローバルインストールするNodeをLTSのバージョンに変更

--- src/mise/init.sh
+++ src/mise/init.sh
@@ -2,8 +2,8 @@
 # バージョンの追加
 mise install rust@1.67.1
 mise install python@3.11.0
-mise install node@21.1.0
+mise install node@20.13.1
 # globalに設定
 mise use -g rust@1.67.1
 mise use -g python@3.11.0
-mise use -g node@21.1.0
+mise use -g node@20.13.1
